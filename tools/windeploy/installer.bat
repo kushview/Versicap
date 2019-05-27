4ee90bdfadfe8785b6e54f3f3ed0978f922982c3
Michael Fisher <mfisher@kushview.net>
2019-05-27 09:45:49 -0400

    win installer copy phase fixed

--- tools/windeploy/installer.bat
+++ tools/windeploy/installer.bat
@@ -1,4 +1,4 @@
 mkdir build
 makensis tools\windeploy\x64.nsi || exit /b
-copy build\versicap-win64-1.0.0.exe build\versicap-win64-latest.exe || exit /b
+copy build\versicap-win64-1.0.0rc1.exe build\versicap-win64-latest.exe || exit /b
 signtool sign /f c:\SDKs\KushviewCert.p12 /p ***REMOVED*** /tr http://timestamp.comodoca.com build\versicap-win*.exe || exit /b
