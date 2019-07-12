ab0fdf16dcc977f44d50ae5c133c1d46f9637603
Michael Fisher <mfisher@kushview.net>
2019-07-12 07:14:03 -0400

    Clean up settings file

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -2,17 +2,41 @@
 
 namespace vcp {
 
+const char* Settings::lastProjectPathKey    = "lastProjectPath";
+
+Settings::Settings()
+{
+    PropertiesFile::Options opts;
+    opts.applicationName     = "Versicap";
+    opts.filenameSuffix      = "conf";
+    opts.osxLibrarySubFolder = "Application Support";
+    opts.storageFormat       = PropertiesFile::storeAsCompressedBinary;
+
+   #if JUCE_DEBUG
+    opts.applicationName << "Debug";
+    opts.storageFormat       = PropertiesFile::storeAsXML;
+   #endif
+    
+   #if JUCE_LINUX
+    opts.folderName          = ".config/versicap";
+   #else
+    opts.folderName          = "Versicap";
+   #endif
+
+    setStorageParameters (opts);
+}
+
 void Settings::setLastProject (const String& path)
 {
     if (auto* props = getUserSettings())
-        props->setValue ("lastProjectPath", path);
+        props->setValue (lastProjectPathKey, path);
 }
 
 File Settings::getLastProject()
 {
     String path;
     if (auto* props = getUserSettings())
-        path = props->getValue ("lastProjectPath");
+        path = props->getValue (lastProjectPathKey);
     return File::isAbsolutePath (path) ? File (path) : File();
 }
 
--- src/Settings.h
+++ src/Settings.h
@@ -7,29 +7,10 @@ namespace vcp {
 class Settings :  public ApplicationProperties
 {
 public:
-    Settings()
-    {
-        PropertiesFile::Options opts;
-        opts.applicationName     = "Versicap";
-        opts.filenameSuffix      = "conf";
-        opts.osxLibrarySubFolder = "Application Support";
-        opts.storageFormat       = PropertiesFile::storeAsCompressedBinary;
+    static const char* lastProjectPathKey;
 
-       #if JUCE_DEBUG
-        opts.applicationName << "Debug";
-        opts.storageFormat       = PropertiesFile::storeAsXML;
-       #endif
-        
-       #if JUCE_LINUX
-        opts.folderName          = ".config/versicap";
-       #else
-        opts.folderName          = "Versicap";
-       #endif
-
-        setStorageParameters (opts);
-    }
-
-    ~Settings() { }
+    Settings();
+    ~Settings() = default;
 
     void setLastProject (const String& path);
     File getLastProject();
